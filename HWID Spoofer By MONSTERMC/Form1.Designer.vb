19c0dcaef098d59814abca8ddd4a14c42a47cee4
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2024-05-09 10:55:19 +0200

    HWID-Spoofer-Test-By-MONSTERMC
    HWID-Spoofer-Test-By-MONSTERMC

---
+++ HWID Spoofer By MONSTERMC/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnGetBIOSInfo = New System.Windows.Forms.Button()
+        Me.btnGetRAMInfo = New System.Windows.Forms.Button()
+        Me.btnGetNetworkAdapterInfo = New System.Windows.Forms.Button()
+        Me.btnGetGPUInfo = New System.Windows.Forms.Button()
+        Me.btnGetCPUInfo = New System.Windows.Forms.Button()
+        Me.btnGetHDDInfo = New System.Windows.Forms.Button()
+        Me.btnGetMotherboardInfo = New System.Windows.Forms.Button()
+        Me.txtBIOSInfo = New System.Windows.Forms.TextBox()
+        Me.txtRAMInfo = New System.Windows.Forms.TextBox()
+        Me.txtNetworkAdapterInfo = New System.Windows.Forms.TextBox()
+        Me.txtGPUInfo = New System.Windows.Forms.TextBox()
+        Me.txtCPUInfo = New System.Windows.Forms.TextBox()
+        Me.txtHDDInfo = New System.Windows.Forms.TextBox()
+        Me.txtMotherboardInfo = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGetBIOSInfo
+        '
+        Me.btnGetBIOSInfo.Location = New System.Drawing.Point(12, 12)
+        Me.btnGetBIOSInfo.Name = "btnGetBIOSInfo"
+        Me.btnGetBIOSInfo.Size = New System.Drawing.Size(146, 23)
+        Me.btnGetBIOSInfo.TabIndex = 0
+        Me.btnGetBIOSInfo.Text = "btnGetBIOSInfo"
+        Me.btnGetBIOSInfo.UseVisualStyleBackColor = True
+        '
+        'btnGetRAMInfo
+        '
+        Me.btnGetRAMInfo.Location = New System.Drawing.Point(164, 12)
+        Me.btnGetRAMInfo.Name = "btnGetRAMInfo"
+        Me.btnGetRAMInfo.Size = New System.Drawing.Size(146, 23)
+        Me.btnGetRAMInfo.TabIndex = 1
+        Me.btnGetRAMInfo.Text = "btnGetRAMInfo"
+        Me.btnGetRAMInfo.UseVisualStyleBackColor = True
+        '
+        'btnGetNetworkAdapterInfo
+        '
+        Me.btnGetNetworkAdapterInfo.Location = New System.Drawing.Point(316, 12)
+        Me.btnGetNetworkAdapterInfo.Name = "btnGetNetworkAdapterInfo"
+        Me.btnGetNetworkAdapterInfo.Size = New System.Drawing.Size(188, 23)
+        Me.btnGetNetworkAdapterInfo.TabIndex = 2
+        Me.btnGetNetworkAdapterInfo.Text = "btnGetNetworkAdapterInfo"
+        Me.btnGetNetworkAdapterInfo.UseVisualStyleBackColor = True
+        '
+        'btnGetGPUInfo
+        '
+        Me.btnGetGPUInfo.Location = New System.Drawing.Point(510, 12)
+        Me.btnGetGPUInfo.Name = "btnGetGPUInfo"
+        Me.btnGetGPUInfo.Size = New System.Drawing.Size(146, 23)
+        Me.btnGetGPUInfo.TabIndex = 3
+        Me.btnGetGPUInfo.Text = "btnGetGPUInfo"
+        Me.btnGetGPUInfo.UseVisualStyleBackColor = True
+        '
+        'btnGetCPUInfo
+        '
+        Me.btnGetCPUInfo.Location = New System.Drawing.Point(12, 41)
+        Me.btnGetCPUInfo.Name = "btnGetCPUInfo"
+        Me.btnGetCPUInfo.Size = New System.Drawing.Size(146, 23)
+        Me.btnGetCPUInfo.TabIndex = 4
+        Me.btnGetCPUInfo.Text = "btnGetCPUInfo"
+        Me.btnGetCPUInfo.UseVisualStyleBackColor = True
+        '
+        'btnGetHDDInfo
+        '
+        Me.btnGetHDDInfo.Location = New System.Drawing.Point(164, 41)
+        Me.btnGetHDDInfo.Name = "btnGetHDDInfo"
+        Me.btnGetHDDInfo.Size = New System.Drawing.Size(146, 23)
+        Me.btnGetHDDInfo.TabIndex = 5
+        Me.btnGetHDDInfo.Text = "btnGetHDDInfo"
+        Me.btnGetHDDInfo.UseVisualStyleBackColor = True
+        '
+        'btnGetMotherboardInfo
+        '
+        Me.btnGetMotherboardInfo.Location = New System.Drawing.Point(316, 41)
+        Me.btnGetMotherboardInfo.Name = "btnGetMotherboardInfo"
+        Me.btnGetMotherboardInfo.Size = New System.Drawing.Size(146, 23)
+        Me.btnGetMotherboardInfo.TabIndex = 6
+        Me.btnGetMotherboardInfo.Text = "btnGetMotherboardInfo"
+        Me.btnGetMotherboardInfo.UseVisualStyleBackColor = True
+        '
+        'txtBIOSInfo
+        '
+        Me.txtBIOSInfo.Location = New System.Drawing.Point(12, 70)
+        Me.txtBIOSInfo.Name = "txtBIOSInfo"
+        Me.txtBIOSInfo.Size = New System.Drawing.Size(146, 20)
+        Me.txtBIOSInfo.TabIndex = 7
+        '
+        'txtRAMInfo
+        '
+        Me.txtRAMInfo.Location = New System.Drawing.Point(164, 70)
+        Me.txtRAMInfo.Name = "txtRAMInfo"
+        Me.txtRAMInfo.Size = New System.Drawing.Size(146, 20)
+        Me.txtRAMInfo.TabIndex = 8
+        '
+        'txtNetworkAdapterInfo
+        '
+        Me.txtNetworkAdapterInfo.Location = New System.Drawing.Point(316, 70)
+        Me.txtNetworkAdapterInfo.Name = "txtNetworkAdapterInfo"
+        Me.txtNetworkAdapterInfo.Size = New System.Drawing.Size(188, 20)
+        Me.txtNetworkAdapterInfo.TabIndex = 9
+        '
+        'txtGPUInfo
+        '
+        Me.txtGPUInfo.Location = New System.Drawing.Point(510, 70)
+        Me.txtGPUInfo.Name = "txtGPUInfo"
+        Me.txtGPUInfo.Size = New System.Drawing.Size(146, 20)
+        Me.txtGPUInfo.TabIndex = 10
+        '
+        'txtCPUInfo
+        '
+        Me.txtCPUInfo.Location = New System.Drawing.Point(12, 96)
+        Me.txtCPUInfo.Name = "txtCPUInfo"
+        Me.txtCPUInfo.Size = New System.Drawing.Size(146, 20)
+        Me.txtCPUInfo.TabIndex = 11
+        '
+        'txtHDDInfo
+        '
+        Me.txtHDDInfo.Location = New System.Drawing.Point(164, 96)
+        Me.txtHDDInfo.Name = "txtHDDInfo"
+        Me.txtHDDInfo.Size = New System.Drawing.Size(146, 20)
+        Me.txtHDDInfo.TabIndex = 12
+        '
+        'txtMotherboardInfo
+        '
+        Me.txtMotherboardInfo.Location = New System.Drawing.Point(316, 96)
+        Me.txtMotherboardInfo.Name = "txtMotherboardInfo"
+        Me.txtMotherboardInfo.Size = New System.Drawing.Size(120, 20)
+        Me.txtMotherboardInfo.TabIndex = 13
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(442, 96)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 20)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Start Spoof Using UFI"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(567, 96)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 20)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "Spoof Via Reg"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(665, 126)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtMotherboardInfo)
+        Me.Controls.Add(Me.txtHDDInfo)
+        Me.Controls.Add(Me.txtCPUInfo)
+        Me.Controls.Add(Me.txtGPUInfo)
+        Me.Controls.Add(Me.txtNetworkAdapterInfo)
+        Me.Controls.Add(Me.txtRAMInfo)
+        Me.Controls.Add(Me.txtBIOSInfo)
+        Me.Controls.Add(Me.btnGetMotherboardInfo)
+        Me.Controls.Add(Me.btnGetHDDInfo)
+        Me.Controls.Add(Me.btnGetCPUInfo)
+        Me.Controls.Add(Me.btnGetGPUInfo)
+        Me.Controls.Add(Me.btnGetNetworkAdapterInfo)
+        Me.Controls.Add(Me.btnGetRAMInfo)
+        Me.Controls.Add(Me.btnGetBIOSInfo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGetBIOSInfo As Button
+    Friend WithEvents btnGetRAMInfo As Button
+    Friend WithEvents btnGetNetworkAdapterInfo As Button
+    Friend WithEvents btnGetGPUInfo As Button
+    Friend WithEvents btnGetCPUInfo As Button
+    Friend WithEvents btnGetHDDInfo As Button
+    Friend WithEvents btnGetMotherboardInfo As Button
+    Friend WithEvents txtBIOSInfo As TextBox
+    Friend WithEvents txtRAMInfo As TextBox
+    Friend WithEvents txtNetworkAdapterInfo As TextBox
+    Friend WithEvents txtGPUInfo As TextBox
+    Friend WithEvents txtCPUInfo As TextBox
+    Friend WithEvents txtHDDInfo As TextBox
+    Friend WithEvents txtMotherboardInfo As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ HWID Spoofer By MONSTERMC/Form1.vb
@@ -0,0 +1,283 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Threading
+Imports System.Text
+Imports System.Management
+Imports Microsoft.Win32
+Public Class Form1
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "spoofer Mad By MONSTERMC test Telegram:@MONSTERMC"
+        Button1.Enabled = False
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DownloadFiles()
+    End Sub
+#Region "Checker"
+    Private Sub btnGetBIOSInfo_Click(sender As Object, e As EventArgs) Handles btnGetBIOSInfo.Click
+        Dim info As String = GetBIOSInfo()
+        txtBIOSInfo.Text = info
+    End Sub
+
+    Private Sub btnGetRAMInfo_Click(sender As Object, e As EventArgs) Handles btnGetRAMInfo.Click
+        Dim info As String = GetRAMInfo()
+        txtRAMInfo.Text = info
+    End Sub
+
+    Private Sub btnGetNetworkAdapterInfo_Click(sender As Object, e As EventArgs) Handles btnGetNetworkAdapterInfo.Click
+        Dim info As String = GetNetworkAdapterInfo()
+        txtNetworkAdapterInfo.Text = info
+    End Sub
+
+    Private Sub btnGetGPUInfo_Click(sender As Object, e As EventArgs) Handles btnGetGPUInfo.Click
+        Dim info As String = GetGPUInfo()
+        txtGPUInfo.Text = info
+    End Sub
+
+    Private Sub btnGetCPUInfo_Click(sender As Object, e As EventArgs) Handles btnGetCPUInfo.Click
+        Dim info As String = GetCPUInfo()
+        txtCPUInfo.Text = info
+    End Sub
+
+    Private Sub btnGetHDDInfo_Click(sender As Object, e As EventArgs) Handles btnGetHDDInfo.Click
+        Dim info As String = GetHDDInfo()
+        txtHDDInfo.Text = info
+    End Sub
+    Private Sub btnGetMotherboardInfo_Click_1(sender As Object, e As EventArgs) Handles btnGetMotherboardInfo.Click
+        Dim info As String = GetMotherboardInfo()
+        txtMotherboardInfo.Text = info
+    End Sub
+    Private Function GetMotherboardInfo() As String
+        Dim info As New StringBuilder()
+
+        Dim searcher As New ManagementObjectSearcher("root\CIMV2", "SELECT * FROM Win32_BaseBoard")
+        For Each queryObj As ManagementObject In searcher.Get()
+            info.AppendLine("Product: " & queryObj("Product"))
+            info.AppendLine("Manufacturer: " & queryObj("Manufacturer"))
+            info.AppendLine("Serial Number: " & queryObj("SerialNumber"))
+        Next
+
+        Return info.ToString()
+    End Function
+
+    Private Function GetBIOSInfo() As String
+        Dim info As New StringBuilder()
+
+        Dim searcher As New ManagementObjectSearcher("root\CIMV2", "SELECT * FROM Win32_BIOS")
+        For Each queryObj As ManagementObject In searcher.Get()
+            info.AppendLine("BIOS Manufacturer: " & queryObj("Manufacturer"))
+            info.AppendLine("BIOS Serial Number: " & queryObj("SerialNumber"))
+            info.AppendLine("BIOS Version: " & queryObj("Version"))
+            info.AppendLine("SMBIOS Version: " & queryObj("SMBIOSBIOSVersion"))
+            info.AppendLine("Release Date: " & queryObj("ReleaseDate"))
+        Next
+
+        Return info.ToString()
+    End Function
+
+    Private Function GetRAMInfo() As String
+        Dim info As New StringBuilder()
+
+        Dim searcher As New ManagementObjectSearcher("root\CIMV2", "SELECT * FROM Win32_PhysicalMemory")
+        For Each queryObj As ManagementObject In searcher.Get()
+            info.AppendLine("Part Number: " & queryObj("PartNumber"))
+            info.AppendLine("Serial Number: " & queryObj("SerialNumber"))
+            info.AppendLine("SKU: " & queryObj("SKU"))
+        Next
+
+        Return info.ToString()
+    End Function
+
+    Private Function GetNetworkAdapterInfo() As String
+        Dim info As New StringBuilder()
+
+        Dim searcher As New ManagementObjectSearcher("root\CIMV2", "SELECT * FROM Win32_NetworkAdapter")
+        For Each queryObj As ManagementObject In searcher.Get()
+            info.AppendLine("MAC Address: " & queryObj("MACAddress"))
+            info.AppendLine("Description: " & queryObj("Description"))
+        Next
+
+        Return info.ToString()
+    End Function
+
+    Private Function GetGPUInfo() As String
+        Dim info As New StringBuilder()
+
+        ' يمكنك استخدام استعلامات WMI للحصول على معلومات عن بطاقة الرسومات هنا
+
+        Return info.ToString()
+    End Function
+
+    Private Function GetCPUInfo() As String
+        Dim info As New StringBuilder()
+
+        Dim searcher As New ManagementObjectSearcher("root\CIMV2", "SELECT * FROM Win32_Processor")
+        For Each queryObj As ManagementObject In searcher.Get()
+            info.AppendLine("Caption: " & queryObj("Caption"))
+            info.AppendLine("Processor ID: " & queryObj("ProcessorID"))
+            info.AppendLine("Socket Designation: " & queryObj("SocketDesignation"))
+            info.AppendLine("Name: " & queryObj("Name"))
+        Next
+
+        Return info.ToString()
+    End Function
+
+    Private Function GetHDDInfo() As String
+        Dim info As New StringBuilder()
+
+        Dim searcher As New ManagementObjectSearcher("root\CIMV2", "SELECT * FROM Win32_DiskDrive")
+        For Each queryObj As ManagementObject In searcher.Get()
+            info.AppendLine("Model: " & queryObj("Model"))
+            info.AppendLine("Serial Number: " & queryObj("SerialNumber"))
+            info.AppendLine("Name: " & queryObj("Name"))
+        Next
+
+        Return info.ToString()
+    End Function
+#End Region
+#Region "Via Reg"
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ExecuteCommandsViaReg()
+    End Sub
+    Private Sub ExecuteCommandsViaReg()
+        Dim commands As String() = {
+            "REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d MonStErmC-%random% /f",
+            "REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName /v ComputerName /t REG_SZ /d MonStErmC-%random% /f",
+            "REG ADD HKLM\SYSTEM\HardwareConfig /v LastConfig /t REG_SZ /d {eac%random%} /f",
+            "REG ADD HKLM\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware\Profiles\0001 /v HwProfileGuid /t REG_SZ /d {MonStErmC-%random%-%random%-%random%-%random%} /f",
+            "REG ADD HKLM\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware\Profiles\0001 /v GUID /t REG_SZ /d {MonStErmC-%random%-%random%-%random%-%random%} /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion /v BuildGUID /t REG_SZ /d MonStErmC-%random% /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion /v RegisteredOwner /t REG_SZ /d MonStErmC-%random% /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion /v RegisteredOrganization /t REG_SZ /d MonStErmC-%random% /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Cryptography /v GUID /t REG_SZ /d MonStErmC-%random%-%random%-%random%-%random% /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Cryptography /v MachineGuid /t REG_SZ /d MonStErmC-%random%-%random%-%random%-%random% /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion /v ProductId /t REG_SZ /d %random%-%random%-%random%-%random% /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion /v InstallDate /t REG_SZ /d %random% /f",
+            "REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SystemInformation /v ComputerHardwareId /t REG_SZ /d {%random%-%random%-%random%-%random%} /f",
+            "REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography /v MachineGuid /t REG_SZ /d %Hex8%-%Hex1%-%Hex0%-%Hex1%-%Hex10% /f",
+            "REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion /v BuildGUID /t REG_SZ /d %Hex8%-%Hex1%-%Hex0%-%Hex1%-%Hex10% /f",
+            "REG ADD HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4d36e967-e325-11ce-bfc1-08002be10318}\Configuration\Variables\BusDeviceDesc /v PropertyGuid /t REG_SZ /d {%Hex8%-%Hex1%-%Hex0%-%Hex1%-%Hex10%} /f",
+            "REG ADD HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\Configuration\Variables\DeviceDesc /v PropertyGuid /t REG_SZ /d {%Hex8%-%Hex1%-%Hex0%-%Hex1%-%Hex10%} /f",
+            "REG ADD HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\Configuration\Variables\Driver /v PropertyGuid /t REG_SZ /d {%Hex8%-%Hex1%-%Hex0%-%Hex1%-%Hex10%} /f",
+            "REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SystemInformation /v ComputerHardwareId /t REG_SZ /d {%Hex8%-%Hex1%-%Hex0%-%Hex1%-%Hex10%} /f",
+            "REG ADD HKLM\Software\Microsoft\Windows NT\CurrentVersion /v InstallDate /t REG_SZ /d %random% /f",
+            "REG ADD HKLM\Software\Microsoft\Windows NT\CurrentVersion /v ProductId /t REG_SZ /d %random% /f",
+            "REG ADD HKLM\System\CurrentControlSet\Control\SystemInformation /v ComputerHardwareId /t REG_SZ /d %random% /f",
+            "REG ADD HKLM\System\CurrentControlSet\Control\WMI\Security /v 671a8285-4edb-4cae-99fe-69a15c48c0bc /t REG_SZ /d %random% /f",
+            "REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v SusClientId /t REG_SZ /d {MonStErmC-%random%-%random%-%random%-%random%} /f",
+            "sc stop winmgmt",
+            "sc start winmgmt",
+            "ipconfig /flushdns"
+        }
+
+        For Each command As String In commands
+            ExecuteCommand(command)
+        Next
+        Button1.Enabled = True
+    End Sub
+
+    Private Sub ExecuteCommand(ByVal command As String)
+        Try
+            Dim processInfo As New ProcessStartInfo()
+            processInfo.FileName = "cmd.exe"
+            processInfo.Arguments = "/c " & command
+            processInfo.WindowStyle = ProcessWindowStyle.Hidden
+            processInfo.CreateNoWindow = True
+            Dim process As New Process()
+            process.StartInfo = processInfo
+            process.Start()
+            process.WaitForExit()
+        Catch ex As Exception
+            MessageBox.Show("Error executing command: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+#End Region
+    ' وهكذا نستمر لباقي الأوامر
+
+    Private Sub DownloadFiles()
+        Try
+            Directory.CreateDirectory("C:\MONSTERMC.syFreePS")
+
+            Dim webClient As New WebClient()
+            webClient.DownloadFile("https://github.com/boxakosidk/files-for-woofer/raw/main/AMIDEWIN.EXE", "C:\MONSTERMC.syFreePS\AMIDEWIN.EXE")
+            webClient.DownloadFile("https://github.com/boxakosidk/files-for-woofer/raw/main/AMIDEWINx64.EXE", "C:\MONSTERMC.syFreePS\AMIDEWINx64.EXE")
+            webClient.DownloadFile("https://github.com/boxakosidk/files-for-woofer/raw/main/AMIFLDRV64.SYS", "C:\MONSTERMC.syFreePS\AMIFLDRV64.SYS")
+            webClient.DownloadFile("https://github.com/boxakosidk/files-for-woofer/raw/main/Volumeid64.exe", "C:\MONSTERMC.syFreePS\Volumeid64.exe")
+
+            MessageBox.Show("Files downloaded successfully.", "Download Complete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            SpoofVolumeID()
+        Catch ex As Exception
+            MessageBox.Show("Error downloading files: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub SpoofVolumeID()
+        Try
+            Dim rand As New Random()
+            Dim rand1 As Integer = rand.Next(1000, 9999)
+            Dim rand2 As Integer = rand.Next(1000, 9999)
+
+            Dim drives As String() = {"c:", "d:", "e:", "f:", "b:", "j:", "i:", "g:", "x:"}
+
+            For Each drive As String In drives
+                Dim process As New Process()
+                process.StartInfo.FileName = "C:\MONSTERMC.syFreePS\Volumeid64.exe"
+                process.StartInfo.Arguments = drive & " " & rand1 & "-" & rand2 & " /accepteula"
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+                process.Start()
+                Thread.Sleep(3000)
+            Next
+
+            MessageBox.Show("Volume IDs spoofed successfully.", "Spoofing Complete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            SpoofBIOS()
+        Catch ex As Exception
+            MessageBox.Show("Error spoofing volume IDs: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub SpoofBIOS()
+        Try
+            Dim processes As String() = {
+                "/BS " & GetRandomSerial(),
+                "/SS " & GetRandomSerial(),
+                "/SV " & GetRandomSerial(),
+                "/SU AUTO",
+                "/SK " & GetRandomSerial(),
+                "/BM " & GetRandomSerial(),
+                "/BV " & GetRandomSerial()
+            }
+
+            For Each processArgs As String In processes
+                Dim process As New Process()
+                process.StartInfo.FileName = "C:\MONSTERMC.syFreePS\AMIDEWIN.EXE"
+                process.StartInfo.Arguments = processArgs
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+                process.Start()
+                Thread.Sleep(3000)
+            Next
+
+            MessageBox.Show("BIOS serials spoofed successfully.", "Spoofing Complete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ' Clean up
+            Directory.Delete("C:\MONSTERMC.syFreePS", True)
+
+            MessageBox.Show("Application completed its tasks successfully.", "Finished", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            Shutdown()
+        Catch ex As Exception
+            MessageBox.Show("Error spoofing BIOS serials: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Function GetRandomSerial() As String
+        Dim rand As New Random()
+        Return rand.Next(1000, 9999) & "u-" & rand.Next()
+    End Function
+
+    Private Sub Shutdown()
+        MessageBox.Show("The system will restart in 10 seconds.", "Restarting", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Process.Start("shutdown", "/r /t 10")
+    End Sub
+End Class
